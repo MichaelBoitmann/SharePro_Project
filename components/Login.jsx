ee73fe40a306dc8d39f7e9f40f1f3ce1b41c9458
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-06-17 00:12:17 +1200

    working on login file

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import { GoogleLogin } from '@react-oauth/google'
-import { useNavigate } from 'react-router-dom'
-import { FcGoogle } from 'react-icons/fc'
+import React from 'react';
+import { GoogleLogin } from '@react-oauth/google';
+import { useNavigate } from 'react-router-dom';
+import { FcGoogle } from 'react-icons/fc';
+import shareVideo from '../assets/share.mp4';
+import logo from '../assets/logowhite.png';
 
 const Login = () => {
   return (
-    <div>
+    <div className='flex justify-start items-center flex-col'>
       Login
     </div>
   )
