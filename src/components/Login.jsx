2d16b0f95166f74056e2e0659ed227b8414085be
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-06-20 21:34:59 +1200

    finalise the basic of new google OAuth 3.o implementation before starting the actual project for designing

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import jwt_decode from 'jwt-decode';
 import { GoogleLoginButton } from 'react-google-button'
 import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
 import { useNavigate } from 'react-router-dom';
@@ -10,11 +11,23 @@ import logo from '../assets/logowhite.png';
 
 const Login = () => {
 
+  const [ user, setUser ] = useState({});
+
   const handleCallbackResponse = (response) => {
     console.log("Encoded JWT ID token: " + response.credential);
+    var userObject = jwt_decode(response.credential);
+    console.log(userObject);
+    setUser(userObject);
+    document.getElementById("signInDiv").hidden = true;
   }
 
-  // 'google' will be called from index.html script before the title
+
+  const handleSignOut = (event) => {
+    setUser();
+    document.getElementById("signInDiv").hidden = false;
+  }
+
+  // google will be called from index.html script before the title
   useEffect(() => {
     
     google.accounts.id.initialize({      
@@ -27,6 +40,7 @@ const Login = () => {
       { theme: "outline", size: "large"}
     )
 
+    google.accounts.id.prompt();
   }, []);
 
   // const responseGoogle=(response) => {
@@ -44,7 +58,7 @@ const Login = () => {
 
   return (
     <div className="flex justify-start items-center flex-col h-screen">
-      <div className="relative w-ful h-full">
+      <div className="relative w-full h-full">
         <video 
           src={shareVideo}
           type="video/mp4"
@@ -54,13 +68,27 @@ const Login = () => {
           autoPlay
           className="w-full h-full object-cover"
         />
-        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay text-orange-100 text-xl">
+          
           <div className="p-5">
             <img src={logo} width="130px" alt="logo" />
           </div>
-          <div className="shadow-2xl">
+          <div >
             <div id="signInDiv"></div>
+            {  Object.keys(user).length != 0 &&
+              <button onClick={ (e) => handleSignOut(e)}>Sign Out</button>
+            }
+            <br />
+            <br />
+
+            { user && 
+              <div>
+                <img src={user.picture}></img>
+                <h3>{user.name}</h3>
+              </div>
+            }
           </div>
+          
         </div>
       </div>
     </div>
