702a42110e85b1465788019ad7c85e6de48c1e1b
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-06-23 23:57:48 +1200

    added files login and sidebars

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { HiMenu } from 'react-icons/hi';
+import { AiFillCloseCircle } from 'react-icons/ai';
+import { Link, Route, Routes } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>HOME</h1>
+    </div>
+  )
+}
+
+export default Home
