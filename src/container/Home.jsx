33b1447360c4175044dd30a2002039be75148aa0
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-06-26 23:18:18 +1200

    working on sidebars and categories

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -5,8 +5,8 @@ import { Link, Route, Routes } from 'react-router-dom';
 
 import { Sidebar, UserProfile } from '../components';
 import { userQuery } from '../utils/data';
-import Pins from './Pins';
 import { client } from '../client';
+import Pins from './Pins';
 import logo from '../assets/sharepro.png';
 
 
@@ -14,8 +14,10 @@ const Home = () => {
 
   const [toggleSidebar, setToggleSidebar] = useState(false);
   const [user, setUser] = useState(null);
+  const scrollRef = useRef(null);
 
   const userInfo = localStorage.getItem('user') != 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+  
 
   useEffect(() => {
     const query = userQuery(userInfo?.googleId);
@@ -27,32 +29,43 @@ const Home = () => {
 
   }, []);
 
+  useEffect(() => {
+    scrollRef.current.scrollTo(0, 0)
+  }, [])
+
 
   return (
-    <div className="flex bg-gray-50 md:flex-row h-screen transition-height duration-75 ease-out">
+    <div className="flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out">
       <div className="hidden md:flex h-screen flex-initial">
-        <Sidebar />
+        <Sidebar user={user && user} />
       </div>
       <div className="flex md:hidden flex-row">
-        <HiMenu fontSize={40} className="cursor-poiter" onClick={() => setToggleSidebar(true)} />
-        <Link to="/">
-          <img src={logo} alt="logo" className="w-28" />
-        </Link>
-        <Link to={`user-profile/${user?._id}`}>
-          <img src={user?.image} alt="logo" className="w-28" />
-        </Link>
-      </div>
-      {toggleSidebar && (
-        <div className="fixed w-4/5 bg-white h-screnn overflow-y-auto shadow-md z-10 animate-slide-in">
-          <div className="absolute w-full flex justify-end items-cent p-2">
-            <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={() => setToggleSidebar(false)}/>
+        <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
+          <HiMenu fontSize={40} className="cursor-pointer" onClick={() => setToggleSidebar(true)} />
+          <Link to="/">
+            <img src={logo} alt="logo" className="w-28" />
+          </Link>
+          <Link to={`user-profile/${user?._id}`}>
+            <img src={user?.image} alt="user-pic" className="w-9 h-9 rounded-full" />
+          </Link>
+        </div>
+        {toggleSidebar && (
+        <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+          <div className="absolute w-full flex justify-end items-center p-2">
+            <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={() => setToggleSidebar(false)} />
           </div>
           <Sidebar user={user && user} closeToggle={setToggleSidebar} />
         </div>
-
-      )}
+        )}
+      </div>
+      <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
+        <Routes>
+          <Route path="/user-profile/:userId" element={<UserProfile />} />
+          <Route path="/*" element={<Pins user={user && user} />} />
+        </Routes>
+      </div>
     </div>
-  )
+  );
 };
 
 export default Home;
