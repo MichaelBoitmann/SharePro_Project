0c5709b74e1aa011089dc8e65d2a2503ca598bb9
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-06-26 23:58:06 +1200

    added few files and update the index.js

--- src/container/Pins.jsx
+++ src/container/Pins.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import { Navbar, Feed, PinDetail, CreatePin, Search } from '../components';
 
 const Pins = () => {
+  const [serachTerm, serSearchTesrm] = useState('')
+
   return (
     <div>
       <h1>PINS</h1>
