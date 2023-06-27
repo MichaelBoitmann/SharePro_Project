809c5a3be22e3fccfc27c87df9607a23b0a3ebcf
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-06-27 17:52:51 +1200

    added rountes on pins file and created the search on navbar

--- src/container/Pins.jsx
+++ src/container/Pins.jsx
@@ -4,10 +4,22 @@ import { Routes, Route } from 'react-router-dom';
 import { Navbar, Feed, PinDetail, CreatePin, Search } from '../components';
 
 const Pins = () => {
-  const [serachTerm, serSearchTesrm] = useState('')
+  const [searchTerm, setSearchTerm] = useState('')
 
   return (
-    <div>
+    <div className="px-2 md:px-5">
+      <div className="bg-gray-50">
+        <Navbar searchTerm={searchTerm} setSearchTerm={searchTerm} />
+      </div>
+      <div className="h-full">
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/category/:categoryId" element={<Feed />} />
+          <Route path="/pin-detail/:pinId" element={<PinDetail />} />
+          <Route path="/" element={<Feed />} />
+          <Route path="/" element={<Feed />} />
+        </Routes>
+      </div>
       <h1>PINS</h1>
     </div>
   )
