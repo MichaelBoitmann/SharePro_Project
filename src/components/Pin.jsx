e107ff955800fbfd63dd87378a81ff68295f3de2
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-07-12 11:57:50 +1200

    working on icon to be stick on each photos to be downloaded

--- src/components/Pin.jsx
+++ src/components/Pin.jsx
@@ -19,18 +19,22 @@ const Pin = ({ pin: { postedBy, image, _id, destination }}) => {
         onMouseEnter={() => setPostHovered(true)}
         onMouseLeave={() => setPostHovered(false)}
         onClick={() => Navigate('/pin-detail/${_id}')}
-        className="relative cursor-zoom-in w-auto"
+        className="relative cursor-zoom-in w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-500 ease-in-out"
       >
-        <img className="rounded-lg w-full" alt="user-post" src={user} />
+        <img className="rounded-lg w-full" alt="user-post" src={(urlFor(image).width(250).url())} />
         {postHoverd && (
           <div
-            className="absolute top-0 w-full h-full flex flex-col"
+            className="absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2 z-50"
             style={{ height: '100%'}}
           >
             <div className="flex items-center justify-between">
               <div className="flex gap-2">
                 <a 
-                  href={`${image?.asset?.url}?dl=`}>
+                  href={`${image?.asset?.url}?dl=`}
+                  download
+                  onClisck={(e) => e.stopPropagation()}
+                >
+                  <MdDownloadForOffline />
 
                 </a>
               </div>
