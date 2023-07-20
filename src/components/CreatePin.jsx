ad364cfd6ec4730b93012a163c5e5052974ae456
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-07-20 13:20:49 +1200

    formatted the create pin window with jsx code

--- src/components/CreatePin.jsx
+++ src/components/CreatePin.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 
 import { client } from '../client';
 import Spinner from './Spinner';
-import { categories } from './utils/data';
+import { categories } from '../utils/data';
 
 
 const CreatePin = ({ user }) => {
@@ -16,10 +16,24 @@ const CreatePin = ({ user }) => {
   const [fields, setFields] = useState(null);
   const [category, setCategory] = useState(null);
   const [imageAsset, setImageAsset] = useState(null);
+  const [wrongImageType, setWrongImageType] = useState(null);
+
+  const navigate = useNavigate();
 
   return (
-    <div>
-      CreatePin
+    <div className="flex flex-col justify-center mt-5 lg:h-4/5">
+      {fields && (
+        <p className="text-red-500 mb-5 text-xl transition-all duration-150 ease-in"
+        >Please fill in all the fields</p>
+      )}
+      <div className="flex lg:flex-row flex-col justify-center items-center bg-white lg:p-5 lg:w-4/5 w-full p-3">
+        <div className="bg-secondColor p-3 flex flex-0.7 w-full">
+          <div className="flex justify-center items-center flex-col border-2 border-dotted border-gray-300 p-3 w-full h-420">
+            {loading && <Spinner />}
+          </div>
+
+        </div>
+      </div>
     </div>
   )
 };
